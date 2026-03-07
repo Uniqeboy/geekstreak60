560106314482a7db032ed21d7d96b8b8bfb0162f
Girjendra <shivaji12889@gmail.com>
2026-03-07 19:14:43 +0530

    Day 4 of geekstreak60

---
+++ d4_count_subarrays_with_given_XOR.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k) {
+        unordered_map<int, int> pre_xor;
+        long ans = 0;
+        int xoc = 0;
+        
+        for(int it : arr) {
+            xoc ^= it;
+            if(xoc == k)
+                ans++;
+                
+            if(pre_xor.find(xoc^k) != pre_xor.end())
+                ans += pre_xor[xoc^k];
+                
+            pre_xor[xoc]++;
+            
+        }
+        return ans;
+    }
+};
