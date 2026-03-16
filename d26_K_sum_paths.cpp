10762f9827eca9853782ffa215e5b17eae5a37d6
Girjendra <shivaji12889@gmail.com>
2026-03-16 20:22:31 +0530

    Day 26 of geekstreak60

--- d26_K_sum_paths.cpp
+++ d26_K_sum_paths.cpp
@@ -1,5 +1,8 @@
 /*
-
+Given the root of a binary tree and an integer k, determine the number of downward-only
+paths where the sum of the node values in the path equals k.
+Note: A path can start and end at any node within the tree but must always move downward
+(from parent to child).
 */
 #include<bits/stdc++.h>
 using namespace std;
